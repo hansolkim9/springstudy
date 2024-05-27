d90c933a09cef4173bf4df5db6cfe05f5daae7fc
hansolkim9 <masicer2@gmail.com>
2024-05-27 09:53:33 +0900

    댓글 API 등록기능 - 입력값 검증

--- src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
@@ -6,9 +6,14 @@ import com.study.springstudy.springmvc.chap05.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/replies")
@@ -41,14 +46,41 @@ public class ReplyApiController {
     // 댓글 생성 요청
     // @RequestBody : 클라이언트가 전송한 데이터를 JSON으로 받아서 파싱
     @PostMapping
-    public ResponseEntity<?> posts(@RequestBody ReplyPostDto dto) {
+    public ResponseEntity<?> posts(
+            @Validated @RequestBody ReplyPostDto dto
+            , BindingResult result // 입력값 검증 결과 데이터를 갖고 있는 객체
+    ) {
+
         log.info("/api/v1/replies : POST");
         log.debug("parameter: {}", dto);
+
+        if (result.hasErrors()) {
+
+            Map<String, String> errors = makeValidationMessageMap(result);
+
+            return ResponseEntity
+                    .badRequest()
+                    .body(result.getAllErrors());
+        }
+
         boolean flag = replyService.register(dto);
 
         if (!flag) return ResponseEntity.internalServerError().body("댓글 등록 실패");
 
         return ResponseEntity.ok().body(replyService.getReplies(dto.getBno()));
+    }
+
+    private Map<String, String> makeValidationMessageMap(BindingResult result) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        // 에러 정보가 모여있는 리스트
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        for (FieldError error : fieldErrors) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return errors;
     }
 
 }
--- src/main/java/com/study/springstudy/springmvc/chap05/dto/request/ReplyPostDto.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/dto/request/ReplyPostDto.java
@@ -2,6 +2,10 @@ package com.study.springstudy.springmvc.chap05.dto.request;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter @ToString
 @NoArgsConstructor
 @AllArgsConstructor
@@ -9,8 +13,18 @@ import lombok.*;
 @Builder
 public class ReplyPostDto {
 
+    /*
+        NotNull : null 허용안됨
+        NotEmpty: null되는데 빈문자 안됨
+        NotBlank: null안되고 빈문자 안됨
+     */
+    @NotBlank
+    @Size(min = 1, max = 300)
     private String text; // 댓글 내용
+    @NotBlank
+    @Size(min = 2, max = 8)
     private String author; // 댓글 작성자
+    @NotNull
     private Long bno; // 원본 글번호
 
 }
