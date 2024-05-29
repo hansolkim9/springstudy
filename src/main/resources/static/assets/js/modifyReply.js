e21e516d521ae314e3d68ac9871590993e2f4af9
hansolkim9 <masicer2@gmail.com>
2024-05-29 14:25:39 +0900

    댓글 수정 2 - 프론트

--- src/main/resources/static/assets/js/modifyReply.js
+++ src/main/resources/static/assets/js/modifyReply.js
@@ -1,7 +1,11 @@
 
 // 수정 이벤트 등록 함수
+import { BASE_URL } from "./reply.js";
+import { fetchInfScrollReplies } from "./getReply.js";
+
 export function modifyReplyClickEvent() {
 
+    // 수정 모드 진입 이벤트
     document.getElementById('replyData').addEventListener('click', e => {
         e.preventDefault();
 
@@ -15,5 +19,46 @@ export function modifyReplyClickEvent() {
         // 모달의 textArea에 넣기
         document.getElementById('modReplyText').value = replyText;
 
+        // 댓글번호 구하기
+        const rno = e.target.closest('#replyContent').dataset.replyId;
+
+        // 모달에 클릭한 댓글번호 달아놓기
+        document.querySelector('.modal').dataset.rno = rno;
+
     });
+
+    // 수정 요청 처리 이벤트
+    document.getElementById('replyModBtn').addEventListener('click', e => {
+
+        fetchReplyModify();
+    });
+}
+
+async function fetchReplyModify() {
+
+    const payload = {
+        rno: document.querySelector('.modal').dataset.rno,
+        newText: document.getElementById('modReplyText').value,
+        bno:document.getElementById('wrap').dataset.bno
+    };
+
+    console.log(payload);
+
+    const res = await fetch(BASE_URL, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+
+    if (!res.ok) {
+        alert('수정 실패!');
+    }
+
+    // 모달 닫기
+    document.getElementById('modal-close').click();
+
+    window.scrollTo(0, 800); // 수정 후 페이지 상단으로 이동
+    await fetchInfScrollReplies();
 }
