1899291b9152e14c1baa12d383e73e8e10f31e95
hansolkim9 <masicer2@gmail.com>
2024-06-05 11:18:40 +0900

    회원가입 - 프로필 사진 처리 2

--- src/main/java/com/study/springstudy/springmvc/chap05/api/MemberController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/MemberController.java
@@ -4,6 +4,7 @@ import com.study.springstudy.springmvc.chap05.dto.request.LoginDto;
 import com.study.springstudy.springmvc.chap05.dto.request.SignUpDto;
 import com.study.springstudy.springmvc.chap05.service.LoginResult;
 import com.study.springstudy.springmvc.chap05.service.MemberService;
+import com.study.springstudy.springmvc.util.FileUtil;
 import com.study.springstudy.springmvc.util.LoginUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,8 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 public class MemberController {
 
+    private String rootPath = "/Users/hansolkim/spring-prj/upload";
+
     private final MemberService memberService;
 
     // 회원가입 양식 열기
@@ -37,6 +40,10 @@ public class MemberController {
     public String signUp(@Validated SignUpDto dto) {
         log.info("/members/sign-up POST ");
         log.debug("params: {}", dto);
+        log.debug("attached profile image name: {}", dto.getProfileImage().getOriginalFilename());
+
+        // 서버에 업로드
+        FileUtil.uploadFile(rootPath, dto.getProfileImage());
 
         boolean flag = memberService.join(dto);
 
